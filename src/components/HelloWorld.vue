309154e51c2647ff51295026688d5ef910952d4a
jocean12 <joe@sheehanweb.com>
2024-02-15 14:32:47 -0600

    update typo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,7 +16,7 @@
           </div>
           <h1>Joe Sheehan</h1>
           <p class="max-800">
-            Hi and welcome to my website. I’m a Web Designer and Developer with a passion to learn new technologies and languages. I’m lucky because my work is also one of my passions. I love solving new problems and finding new ways to solve old problems. I enjoy managing teams and seeing project go from concept to completion.  Along with developing and programming I enjoy surfing, photography and spending time with my amazing wife and children.
+            Hi and welcome to my website. I’m a Web Designer and Developer with a passion to learn new technologies and languages. I’m lucky because my work is also one of my passions. I love solving new problems and finding new ways to solve old problems. I enjoy managing teams and seeing projects go from concept to completion.  Along with developing and programming I enjoy surfing, photography and spending time with my amazing wife and children.
           </p>
         </div>
       </div>
